f85e56a431ff16d633b9e64d5956288e8d0a3add
aliasaad22 <ali.asaad1@student.ehb.be>
2025-06-11 14:41:50 +0200

    rsponsive menu makken

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
+console.log("hello world");
 
-console.log("hello world")
+document.addEventListener("DOMContentLoaded", () => {
+	const burgerMenu = document.getElementById("burgerMenu");
+	const menu = document.getElementById("menu");
+
+	if (burgerMenu && menu) {
+		burgerMenu.addEventListener("click", () => {
+			burgerMenu.classList.toggle("active");
+			menu.classList.toggle("active");
+		});
+	}
+});
