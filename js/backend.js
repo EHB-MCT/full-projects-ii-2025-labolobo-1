fdde178c17a78af2958441830812a12088dd8416
JoachimGautama <joachim.gautama@student.ehb.be>
2025-06-16 16:23:54 +0200

    Fix: fetcher function to handle response and errors properly

--- js/backend.js
+++ js/backend.js
@@ -5,5 +5,10 @@ function init() {
 }
 
 async function fetcher() {
-  fetch(URL);
+  const rep = await fetch(URL);
+  if (!Response.ok) {
+    throw new Error(`response: ${rep.status}`);
+  }
+
+  const json = await rep.json();
 }
