ce19fbea79045bd7f1c386551cb0b65050034ead
JoachimGautama <joachim.gautama@student.ehb.be>
2025-06-18 11:07:14 +0200

    feat: update backend.js to refactor fetch logic and improve variable naming

--- js/backend.js
+++ js/backend.js
@@ -1,16 +1,20 @@
 const URL = `http://localhost:8090/api/collections`;
+import Project from "./classes/Project.js";
+import Txt from "./classes/Txt.js";
 // if you can't access it: you need my IP, doofus
 // might change, so ask if it doesn' work
 // address: 10.2.88.244:8090/_/
 // you know where to find the login
 
 const projects = `/projects/records`;
-const txt = `/txt/records`;
+const base = `/txt/records`; //base is the basetxt for the website
 const articles = `/articles/records`;
+const team = `/team/records`;
 
-let baseTxt = [];
+let baseTxt = []; //this is the only one that doesn't contain any imgs
 let projectTxt = [];
 let articleTxt = [];
+let teamTxt = [];
 
 let language = navigator.language;
 
@@ -28,7 +32,7 @@ function init() {
 
   language = localStorage.getItem("lang");
 
-  fetcher(txt);
+  fetcher(base);
 }
 
 async function fetcher(option) {
@@ -36,6 +40,8 @@ async function fetcher(option) {
 
   const json = await rep.json();
   console.log(json.items);
+  json.items.forEach(function (e) {});
+
   // fetch everything, put in classes
   // check if we can use `${language}` in the calls
 }
