ddca6c16056ad35504eac7dfc367fba59d2618cf
JoachimGautama <joachim.gautama@student.ehb.be>
2025-06-18 10:45:44 +0200

    feat: refactor backend.js to improve fetch functionality and remove unused classes

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,16 @@
 const URL = `http://localhost:8090/api/collections`;
+// if you can't access it: you need my IP, doofus
+// might change, so ask if it doesn' work
+// address: 10.2.88.244:8090/_/
+// you know where to find the login
+
 const projects = `/projects/records`;
 const txt = `/txt/records`;
 const articles = `/articles/records`;
+
+let baseTxt = [];
+let projectTxt = [];
+let articleTxt = [];
 
 let language = navigator.language;
 
@@ -19,11 +28,11 @@ function init() {
 
   language = localStorage.getItem("lang");
 
-  fetcher();
+  fetcher(txt);
 }
 
-async function fetcher() {
-  const rep = await fetch(URL + projects);
+async function fetcher(option) {
+  const rep = await fetch(URL + option);
 
   const json = await rep.json();
   console.log(json.items);
